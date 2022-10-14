58cd7f57b02e2d54e3553d78d24ba291b23893a9
bekhzod <bexzod9834@gmail.com>
2022-10-14 14:58:46 +0900

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Bekhzod Akhmedov on 2022/10/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
